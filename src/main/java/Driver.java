0a52c54c06909e636095f55993c1a348474fb911
spidey143 <bekenev.daniilkpc582020@gmail.com>
2024-03-18 20:33:51 +0300

    Initial commit

---
+++ src/main/java/Driver.java
@@ -0,0 +1,4 @@
+import io.appium.java_client.android.AndroidDriver;
+
+public class Driver extends AndroidDriver {
+}
---
+++ src/test/java/ExampleTestOzon.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class ExampleTestOzon {
+
+    @Test
+    public void test1() {
+
+    }
+}
