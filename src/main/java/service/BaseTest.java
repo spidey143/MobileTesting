85fa20f0c861074e4097e2c3ffc6a02ae91ddb15
spidey143 <bekenev.daniilkpc582020>
2024-03-29 17:27:59 +0300

    Почти готово

--- src/main/java/service/BaseTest.java
+++ src/main/java/service/BaseTest.java
@@ -6,17 +6,23 @@ import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.appium.SelenideAppium;
 import io.appium.java_client.android.AndroidDriver;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
 
-    @BeforeClass(description = "Инициализация")
-    public void setUp(){
+    @BeforeClass(description = "Настройка конфигурации")
+    public void configuring(){
         Configuration.browser = Driver.class.getName();
-        SelenideAppium.launchApp();
     };
 
-    @AfterClass(description = "Завершение")
+    @BeforeMethod(description = "Инициализация")
+    public void setUp(){
+        SelenideAppium.launchApp();
+    }
+
+    @AfterMethod(description = "Завершение")
     public void tearDown(){
         Driver.closeDriver();
     };
--- src/test/java/ProductTests.java
+++ src/test/java/ProductTests.java
@@ -1,7 +1,10 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.appium.AppiumScrollOptions;
+import com.codeborne.selenide.appium.ScrollDirection;
 import com.codeborne.selenide.appium.SelenideAppium;
 import io.appium.java_client.AppiumBy;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import service.BaseTest;
 
@@ -14,13 +17,13 @@ public class ProductTests extends BaseTest {
     @Test(description = "Добавление товара в корзину")
     public void addProductToCart() {
         //Тапнуть на поле с поиском
-        $(AppiumBy.xpath("//android.widget.TextView[@resource-id=\"ru.ozon.app.android:id/searchTv\"]"))
+        $(AppiumBy.id("ru.ozon.app.android:id/searchTv"))
                 .hideKeyboard()
                 .shouldBe(Condition.visible)
                 .click(tap());
 
         //Ввести значение "iphone 15" в поле поиска
-        $(AppiumBy.id("//android.widget.EditText[@resource-id=\"ru.ozon.app.android:id/etSearch\"]"))
+        $(AppiumBy.id("ru.ozon.app.android:id/etSearch"))
                 .hideKeyboard()
                 .shouldBe(Condition.visible)
                 .setValue("iphone 15");
@@ -34,24 +37,39 @@ public class ProductTests extends BaseTest {
         * Apple Смартфон Смартфон Apple Iphone 15 128 ГБ китайской версии с дисплеем Super Retina XDR диагональю 6,1 дюйма
         *  и бионическим чипом 16 iOS 16 CN 6/128 ГБ, черный"
         * */
-        $$(AppiumBy.id("ru.ozon.app.android:id/tileGridItemCl"))
-                .first()
-                .scrollTo()
+        String s = Selenide.$(AppiumBy.androidUIAutomator(
+                "new UiScrollable(new UiSelector().scrollable(true)" +
+                        ".instance(0)).scrollIntoView(new UiSelector().textContains(\"Apple Iphone 15\").instance(0))")).getText();
+        SelenideAppium.$(AppiumBy.androidUIAutomator(
+                        "new UiScrollable(new UiSelector().scrollable(true)" +
+                                ".instance(0)).scrollIntoView(new UiSelector().textContains(\"Apple Iphone 15\").instance(0))"))
+                .scroll(AppiumScrollOptions.with(ScrollDirection.DOWN, 2))
                 .click(tap());
+        if(Selenide.$(AppiumBy.id("ru.ozon.app.android:id/onboardingV")).isDisplayed()) SelenideAppium.back();
 
         //Тапнуть на кнопку "В корзину"
-        $(AppiumBy.xpath("//android.view.ViewGroup[@resource-id=\"ru.ozon.app.android:id/mainBtn\"]"))
-                .shouldBe(Condition.visible).click(tap());
+        Selenide.$(AppiumBy.id("ru.ozon.app.android:id/mainBtn"))
+                .shouldBe(Condition.visible)
+                .click(tap());
+
+        Selenide.$(AppiumBy.xpath("//android.view.ViewGroup[@content-desc=\"Корзина\"]"))
+                .click(tap());
+
+        //TODO поменять ассерт
+        Assert.assertTrue(
+                Selenide.$(AppiumBy.xpath("//android.widget.TextView[@text='"+s+"']"))
+                        .isDisplayed(), "Товар не был добавлен в корзину"
+        );
     }
 
     @Test(description = "Удаление товара из корзины")
     public void deleteProductOfCart() {
         //Тапнуть на поле с поиском
-        Selenide.$(AppiumBy.xpath("//android.widget.TextView[@resource-id=\"ru.ozon.app.android:id/searchTv\"]"))
+        Selenide.$(AppiumBy.id("ru.ozon.app.android:id/searchTv"))
                 .shouldBe(Condition.visible)
                 .click(tap());
         //Ввести значение "iphone 15" в поле поиска
-        Selenide.$(AppiumBy.xpath("//android.widget.EditText[@resource-id=\"ru.ozon.app.android:id/etSearch\"]"))
+        Selenide.$(AppiumBy.id("ru.ozon.app.android:id/etSearch"))
                 .shouldBe(Condition.visible)
                 .sendKeys("iphone 15");
         //Тапнуть на предложенный вариант "iphone 15"
@@ -62,34 +80,35 @@ public class ProductTests extends BaseTest {
         String s = Selenide.$(AppiumBy.androidUIAutomator(
                         "new UiScrollable(new UiSelector().scrollable(true)" +
                                 ".instance(0)).scrollIntoView(new UiSelector().textContains(\"Apple Iphone 15\").instance(0))")).getText();
-        Selenide.$(AppiumBy.androidUIAutomator(
+        SelenideAppium.$(AppiumBy.androidUIAutomator(
                 "new UiScrollable(new UiSelector().scrollable(true)" +
                         ".instance(0)).scrollIntoView(new UiSelector().textContains(\"Apple Iphone 15\").instance(0))"))
+                .scroll(AppiumScrollOptions.with(ScrollDirection.DOWN,1))
                 .click(tap());
 
-        if(Selenide.$(AppiumBy.id("ru.ozon.app.android:id/onboardingV")).exists()) SelenideAppium.back();
+        if(Selenide.$(AppiumBy.id("ru.ozon.app.android:id/onboardingV")).isDisplayed()) SelenideAppium.back();
 
         //Тапнуть на кнопку "Добавить в корзину"
-        Selenide.$(AppiumBy.id("ru.ozon.app.android:id/mainBtn"))
+        SelenideAppium.$(AppiumBy.id("ru.ozon.app.android:id/mainBtn"))
                 .shouldBe(Condition.visible)
-                .click();
+                .click(tap());
 
         Selenide.$(AppiumBy.xpath("//android.view.ViewGroup[@content-desc=\"Корзина\"]"))
                 .click(tap());
 
-        System.out.println(
-        $$(AppiumBy.xpath("//android.widget.TextView"))
-                .find(Condition.text(s)).getText());
         //да я ни ебу почему ancestor не работает
         Selenide.$(AppiumBy.xpath("//android.widget.TextView[@text='"+s+"']/../../../.."))
                 .find(AppiumBy.id("ru.ozon.app.android:id/removeButton"))
                 .click(tap());
-        /*
-        //Удалить товар из корзины
-        $$(AppiumBy.xpath("//android.widget.TextView"))
-                .findBy(Condition.text("Apple Смартфон Смартфон Apple Iphone 15 128 ГБ китайской версии с дисплеем Super Retina XDR диагональю 6,1 дюйма и бионическим чипом 16 iOS 16 CN 6/128 ГБ, черный"))
-                .ancestor("[resource-id=ru.ozon.app.android:id/splitV2ItemRootCl]")
-                .find(AppiumBy.id("ru.ozon.app.android:id/removeButton"))
-                .click(tap());*/
+
+        SelenideAppium.$(AppiumBy.xpath("//android.widget.Button[@text='УДАЛИТЬ ТОВАР']"))
+                .shouldBe(Condition.visible)
+                .click(tap());
+
+        //TODO поменять ассерт
+        Assert.assertTrue(
+                Selenide.$(AppiumBy.xpath("//android.widget.TextView[@text='"+s+"']"))
+                .isDisplayed(), "Товар не был удален из корзины"
+        );
     }
 }
